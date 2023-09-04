5bd9647478228e013f9fc413a99cb8fbaed1a8e0
hazhirmohammadi <hazhir6690@gmail.com>
2023-09-04 12:05:32 +0330

    Dynamization and finishing

--- app/quiz/page.js
+++ app/quiz/page.js
@@ -8,7 +8,7 @@ export default function Page() {
    const [selectedAnswer, setSelectedAnswer] = useState("");
    const [checked, setChecked] = useState(false);
    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
-   const [showResult, setShowResult] = useState(true);
+   const [showResult, setShowResult] = useState(false);
    const [result, setResult] = useState({
       score: 0,
       correctAnswers: 0,
@@ -18,10 +18,54 @@ export default function Page() {
    const { questions } = quiz;
    const { answers, correctAnswer } = questions[activeQuestion];
 
+   // Select And Check
+   const onAnswerSelected = (answer, index) => {
+      setChecked(true);
+      setSelectedAnswerIndex(index);
+
+      if (answer === correctAnswer) {
+         setSelectedAnswer(true);
+         console.log("True answer");
+      } else {
+         setSelectedAnswer(false);
+         console.log("False answer");
+      }
+   };
+
+   // Calculate score and increment to next question
+   const nextQuestion = () => {
+      setSelectedAnswerIndex(null);
+      setResult((prev) =>
+          selectedAnswer
+              ? {
+                 ...prev,
+                 score: prev.score + 5,
+                 correctAnswers: prev.correctAnswers + 1,
+              }
+              : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+      );
+
+      if (activeQuestion !== questions.length - 1) {
+         setActiveQuestion((prev) => prev + 1);
+      } else {
+         setActiveQuestion(0);
+         setShowResult(true);
+      }
+
+      setChecked(false);
+   };
+
    return (
        <div className="container">
           <h1>صفحه آزمون</h1>
-          <div>{/* Quiz count*/}</div>
+          <div>
+             {!showResult ? (
+                 <h2>
+                    آزمون : {activeQuestion + 1} از{" "}
+                    <span>{questions.length}</span>
+                 </h2>
+             ) : null}
+          </div>
           <div>
              {!showResult ? (
                  <div className="quiz-container">
@@ -29,7 +73,7 @@ export default function Page() {
                     {answers.map((answer, index) => (
                         <li
                             key={index}
-                            onClick={() => {}}
+                            onClick={() => onAnswerSelected(answer, index)}
                             className={
                                selectedAnswerIndex === index
                                    ? "li-selected"
@@ -40,7 +84,7 @@ export default function Page() {
                         </li>
                     ))}
                     {checked ? (
-                        <button className="btn" onClick={() => {}}>
+                        <button className="btn" onClick={nextQuestion}>
                            {activeQuestion === questions.length - 1
                                ? "پایان"
                                : "بعدی"}
@@ -48,7 +92,7 @@ export default function Page() {
                     ) : (
                         <button
                             className="btn-disabled"
-                            onClick={() => {}}
+                            onClick={nextQuestion}
                             disabled
                         >
                            {activeQuestion === questions.length - 1
